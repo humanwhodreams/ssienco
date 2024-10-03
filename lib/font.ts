61f8bab61663228becd46a820792777a1b3d3ef1
enoabasi <watermelonbit@gmail.com>
2024-10-03 19:11:52 +0100

    feat: boilerplate cleanup

---
+++ lib/font.ts
@@ -0,0 +1,13 @@
+import localFont from "next/font/local";
+
+const geistSans = localFont({
+  src: "../public/fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+
+const geistMono = localFont({
+  src: "../public/fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
---
+++ lib/metadata.ts
@@ -0,0 +1,8 @@
+import type { Metadata } from 'next';
+
+export const generateMetadata = (): Metadata => {
+  return {
+    title: 'Nextjs Blog',
+    description: 'Ready made blog site for everyone, powered by Markdown.',
+  };
+};
