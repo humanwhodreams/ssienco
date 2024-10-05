e54b24d76332f2f3a076d4b51c9b8ba8e27bcc21
enoabasi <watermelonbit@gmail.com>
2024-10-05 21:17:57 +0100

    refac: blog page
    - add `published` flag for displaying posts
    - fetch sorted posts in `getData()` based on date/time

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,40 +1,49 @@
+import * as React from 'react';
+
 import Link from 'next/link';
 import { blog } from '@/app/source';
 import { cn } from '@/lib/cn';
 import { focusRing } from '@/lib/focuses';
 
-export default function Page() {
-  const data = [...blog.getPages()].sort(
+type Blog = typeof blog;
+
+function getData(content: Blog) {
+  return [...content.getPages()].sort(
     (a, b) =>
       new Date(b.data.date ?? b.file.name).getTime() -
       new Date(a.data.date ?? a.file.name).getTime()
   );
+}
+
+export default function Page() {
+  const data = getData(blog);
 
   return (
     <>
-      <section className="h-[300px] p-8 md:h-[400px] md:p-12 bg-tertiary text-tertiary-foreground">
+      <section className="h-[300px] p-8 md:h-[400px] md:p-12 border">
         <h1>Blog</h1>
-        <p className='text-xl'>Light and gorgeous. like the moon</p>
+        <p className="text-xl">Light and gorgeous. like the moon</p>
       </section>
       <section className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
         {data.map((post) => (
-          <Link
-            key={post.url}
-            href={post.url}
-            className={cn(
-              'flex flex-col p-4 transition-colors border bg-card hover:bg-accent hover:text-accent-foreground focus-visible:ring-offset-0',
-              focusRing
-            )}
-          >
-            <p className="font-medium">{post.data.title}</p>
-            <p className="muted">
-              {post.data.description}
-            </p>
+          <React.Fragment key={post.url}>
+            {post.data.published ? (
+              <Link
+                href={post.url}
+                className={cn(
+                  'flex flex-col p-4 transition-colors border border-t-0 bg-card hover:bg-accent hover:text-accent-foreground focus-visible:ring-offset-0',
+                  focusRing
+                )}
+              >
+                <p className="font-medium">{post.data.title}</p>
+                <p className="muted">{post.data.description}</p>
 
-            <p className="pt-4 mt-auto text-xs text-muted-foreground">
-              {new Date(post.data.date ?? post.file.name).toDateString()}
-            </p>
-          </Link>
+                <p className="pt-4 mt-auto text-xs text-muted-foreground">
+                  {new Date(post.data.date ?? post.file.name).toDateString()}
+                </p>
+              </Link>
+            ) : null}
+          </React.Fragment>
         ))}
       </section>
     </>
