cd9450e1d5aef77130b9e2098f01d9c1dcf989e0
enoabasi <watermelonbit@gmail.com>
2024-10-05 21:14:00 +0100

    refac: remove unused collections (docs, meta)

--- source.config.ts
+++ source.config.ts
@@ -1,28 +1,13 @@
 import {
   defineCollections,
   defineConfig,
-  defineDocs,
   frontmatterSchema,
-  metaSchema,
 } from 'fumadocs-mdx/config';
 
+import { rehypeCode } from 'fumadocs-core/mdx-plugins';
 import { remarkImage } from 'fumadocs-core/mdx-plugins';
+import { remarkInstall } from 'fumadocs-docgen';
 import { z } from 'zod';
-
-export const { docs, meta } = defineDocs({
-  // MD/MDX file definitions
-  docs: {
-    schema: frontmatterSchema.extend({
-      published: z.boolean().default(true),
-    }),
-  },
-  // JSON/YAML file definitions
-  meta: {
-    schema: metaSchema.extend({
-      description: z.string().optional(),
-    }),
-  },
-});
 
 export const blog = defineCollections({
   type: 'doc',
@@ -30,11 +15,14 @@ export const blog = defineCollections({
   schema: frontmatterSchema.extend({
     author: z.string(),
     date: z.string().or(z.date()).optional(),
+    published: z.boolean().default(true),
+    category: z.string(),
   }),
 });
 
 export default defineConfig({
   mdxOptions: {
-    remarkPlugins: [remarkImage],
+    rehypePlugins: [rehypeCode],
+    remarkPlugins: [remarkImage, remarkInstall],
   },
 });
