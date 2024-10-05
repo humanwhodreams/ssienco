be10a5ccf03e0d1b14cc574c04206d974d52c135
enoabasi <watermelonbit@gmail.com>
2024-10-05 10:41:26 +0100

    remove manual mdx tooling

--- lib/metadata.ts
+++ lib/metadata.ts
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 
 export const generateMetadata = (): Metadata => {
   return {
-    title: 'Nextjs Blog',
+    title: 'Blog',
     description: 'Ready made blog site for everyone, powered by Markdown.',
   };
 };
