f0219ba0f4b585af384367a81484d10e1020a7a0
enoabasi <watermelonbit@gmail.com>
2024-10-13 23:34:24 +0100

    fix: empty string on next config

--- next.config.mjs
+++ next.config.mjs
@@ -5,16 +5,6 @@ const withMdx = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'github.githubassets.com',
-        port: '',
-        pathname: '',
-      },
-    ],
-  },
 };
 
 export default withMdx(config);
